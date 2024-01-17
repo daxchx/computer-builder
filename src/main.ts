6c57d67eb7b5715c11dcb0e91aa84c73605b87ce
daxchx <daxchx.v1@gmail.com>
2024-01-18 00:39:07 +0900

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <p class="text-3xl font-bold underline">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`;
