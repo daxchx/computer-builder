f678f4f35a62b477fae62d56c6773a00616bfd01
daxchx <daxchx.v1@gmail.com>
2024-01-20 23:50:59 +0900

    Add config.ts file

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  url: 'https://api.recursionist.io/builder/computers',
+  cpuBrand: document.querySelector<HTMLSelectElement>('#cpu-brand'),
+  cpuModel: document.querySelector<HTMLSelectElement>('#cpu-model'),
+  gpuBrand: document.querySelector<HTMLSelectElement>('#gpu-brand'),
+  gpuModel: document.querySelector<HTMLSelectElement>('#gpu-model'),
+  ramNum: document.querySelector<HTMLSelectElement>('#ram-num'),
+  ramBrand: document.querySelector<HTMLSelectElement>('#ram-brand'),
+  ramModel: document.querySelector<HTMLSelectElement>('#ram-model'),
+}
+
+export interface CpuDataType {
+  Type: string
+  Part_Number: number
+  Brand: string
+  Model: string
+  Rank: number
+  Benchmark: number
+}
