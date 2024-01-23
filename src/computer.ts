9fd1a991f1897905074efec083b076d5a94291b5
daxchx <daxchx.v1@gmail.com>
2024-01-24 03:02:04 +0900

    Make the set data method more generic

--- src/computer.ts
+++ src/computer.ts
@@ -1,9 +1,11 @@
+import FetchDataType from './type'
+
 export default class Computer {
-  public cpu: null
-  public gpu: null
-  public ram: null
-  public hdd: null
-  public ssd: null
+  public cpu: FetchDataType | null
+  public gpu: FetchDataType | null
+  public ram: FetchDataType | null
+  public hdd: FetchDataType | null
+  public ssd: FetchDataType | null
 
   constructor() {
     this.cpu = null
@@ -13,7 +15,29 @@ export default class Computer {
     this.ssd = null
   }
 
-  public setCpu(data: any): void {
-    this.cpu = data
+  public setParts(parts: string, data: FetchDataType): void {
+    switch (parts) {
+      case 'cpu':
+        this.cpu = data
+        break
+      case 'gpu':
+        this.gpu = data
+        break
+      case 'ram':
+        this.ram = data
+        break
+      case 'hdd':
+        this.hdd = data
+        break
+      case 'ssd':
+        this.ssd = data
+        break
+      default:
+        console.log('not found')
+    }
+  }
+
+  public getBenchMark(data: any): number {
+    return data.Benchmark
   }
 }
