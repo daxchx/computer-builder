74b9d0916cd088b114d3324f2686c74562d5d978
daxchx <daxchx.v1@gmail.com>
2024-01-23 23:08:21 +0900

    Add gpu selector fetch data

--- src/controller.ts
+++ src/controller.ts
@@ -14,6 +14,7 @@ export default class Controller {
   public init(): void {
     let cpuData: any | null = null
     let gpuData: any | null = null
+    let ramData: any | null = null
 
     fetch(config.url + 'cpu')
       .then((response) => response.json())
@@ -45,6 +46,27 @@ export default class Controller {
         }
       })
 
+    fetch(config.url + 'ram')
+      .then((response) => response.json())
+      .then((data) => {
+        ramData = data
+        let ramNumArr: number[] = []
+        for (let ramNum of data) {
+          for (let i = ramNum.Model.length - 1; i >= 0; i--) {
+            if (ramNum.Model[i] === ' ') {
+              if (ramNumArr.indexOf(ramNum.Model[i + 1]) === -1) {
+                ramNumArr.push(ramNum.Model[i + 1])
+              }
+              break
+            }
+          }
+        }
+        ramNumArr.sort((a, b) => (a < b ? -1 : 1))
+        for (let num of ramNumArr) {
+          this.view.generateRamNum(num)
+        }
+      })
+
     config.cpuBrand?.addEventListener('change', () => {
       config.cpuModel!.innerHTML = `<option value="-">-</option>`
       for (let cpu of cpuData) {
@@ -62,5 +84,115 @@ export default class Controller {
         }
       }
     })
+
+    config.ramNum?.addEventListener('change', () => {
+      config.ramBrand!.innerHTML = `<option value="-">-</option>`
+      let ramBrand: string[] = []
+      for (let ram of ramData) {
+        if (ramBrand.indexOf(ram.Brand) === -1) {
+          ramBrand.push(ram.Brand)
+        }
+      }
+      for (let brand of ramBrand) {
+        this.view.generateRamBrand(brand)
+      }
+    })
   }
 }
+
+// // memory
+
+// let ramData: any = []
+
+// fetch(config.url + '?type=ram')
+//   .then((response) => response.json())
+//   .then(function (data) {
+//     for (let ram of data) {
+//       ramData.push(ram)
+//       generateRamNum(ram)
+//     }
+//     generateRamNumOption(ramNumArray)
+//   })
+
+// let ramNumArray: number[] = []
+
+// function generateRamNum(data: any): void {
+//   for (let i = data.Model.length - 1; i >= 0; i--) {
+//     if (data.Model[i] === ' ') {
+//       if (ramNumArray.indexOf(data.Model[i + 1]) === -1) {
+//         ramNumArray.push(data.Model[i + 1])
+//         return
+//       } else {
+//         return
+//       }
+//     }
+//   }
+// }
+
+// function generateRamNumOption(data: number[]): void {
+//   data.map((num) => {
+//     let option = document.createElement('option')
+//     option.setAttribute('value', num.toString())
+//     option.innerText = num.toString()
+//     config.ramNum?.append(option)
+//   })
+// }
+// let ramBrandArray: string[] = []
+
+// function generateRamBrand(data: any) {
+//   if (ramBrandArray.indexOf(data.Brand) === -1) {
+//     ramBrandArray.push(data.Brand)
+//     return
+//   } else {
+//     return
+//   }
+// }
+
+// function generateRamBrandOption(data: string[]): void {
+//   data.map((brand) => {
+//     let option = document.createElement('option')
+//     option.setAttribute('value', brand)
+//     option.innerText = brand
+//     config.ramBrand?.append(option)
+//   })
+// }
+
+// config.ramNum!.addEventListener('change', () => {
+//   for (let ram of ramData) {
+//     generateRamBrand(ram)
+//   }
+//   config.ramBrand!.innerHTML = ''
+//   config.ramModel!.innerHTML = ''
+//   config.ramBrand!.innerHTML = `<option value="">-</option>`
+//   config.ramModel!.innerHTML = `<option value="">-</option>`
+//   generateRamBrandOption(ramBrandArray)
+// })
+
+// config.ramBrand!.addEventListener('change', () => {
+//   config.ramModel!.innerHTML = ''
+//   config.ramModel!.innerHTML = `<option value="">-</option>`
+//   for (let ram of ramData) {
+//     generateRamModel(ram)
+//   }
+// })
+
+// function generateRamModel(data: any) {
+//   if (config.ramBrand?.value == data.Brand) {
+//     if (config.ramNum?.value == findEmptyString(data)) {
+//       let option = document.createElement('option')
+//       option.value = data.Model
+//       option.innerText = data.Model
+//       config.ramModel?.append(option)
+//     }
+//   }
+// }
+
+// function findEmptyString(data: any): string {
+//   let stringArray: string[] = []
+//   for (let i = data.Model.length; i >= 0; i--) {
+//     if (data.Model[i] !== ' ') {
+//       stringArray.push(data.Model[i])
+//     } else break
+//   }
+//   return stringArray.pop() as string
+// }
