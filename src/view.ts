c4bbeacc17f93bd2af9256548f1eec11436c6ea5
daxchx <daxchx.v1@gmail.com>
2024-01-24 03:02:58 +0900

    Make the generate element method more generic

--- src/view.ts
+++ src/view.ts
@@ -1,45 +1,8 @@
-import { config } from './config'
-
 export default class View {
-  public generateCpuBrand(brand: string): void {
+  public generateElement(target: HTMLSelectElement, data: string): void {
     let element = document.createElement('option')
-    element.value = brand
-    element.innerText = brand
-    config.cpuBrand?.append(element)
-  }
-
-  public generateCpuModel(model: string): void {
-    let element = document.createElement('option')
-    element.value = model
-    element.innerText = model
-    config.cpuModel?.append(element)
-  }
-
-  public generateGpuBrand(brand: string): void {
-    let element = document.createElement('option')
-    element.value = brand
-    element.innerText = brand
-    config.gpuBrand?.append(element)
-  }
-
-  public generateGpuModel(model: string): void {
-    let element = document.createElement('option')
-    element.value = model
-    element.innerText = model
-    config.gpuModel?.append(element)
-  }
-
-  public generateRamNum(num: number): void {
-    let element = document.createElement('option')
-    element.value = num.toString()
-    element.innerText = num.toString()
-    config.ramNum?.append(element)
-  }
-
-  public generateRamBrand(brand: string): void {
-    let element = document.createElement('option')
-    element.value = brand
-    element.innerText = brand
-    config.ramBrand?.append(element)
+    element.value = data
+    element.innerText = data
+    target.append(element)
   }
 }
