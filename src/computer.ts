ef94b3f4d8634599bd3e0322a11ae5d16ee990da
daxchx <daxchx.v1@gmail.com>
2024-01-25 22:31:55 +0900

    Add multiple methods

--- src/computer.ts
+++ src/computer.ts
@@ -1,43 +1,109 @@
 import FetchDataType from './type'
 
+interface CpuDataType {
+  brand: string | null
+  model: string | null
+  benchmark: number | null
+}
+
+interface StorageDataType {
+  type: string | null
+  brand: string | null
+  model: string | null
+  benchmark: number | null
+}
+
 export default class Computer {
-  public cpu: FetchDataType | null
-  public gpu: FetchDataType | null
-  public ram: FetchDataType | null
-  public hdd: FetchDataType | null
-  public ssd: FetchDataType | null
+  private cpu: CpuDataType
+  private gpu: CpuDataType
+  private ram: CpuDataType
+  private storage: StorageDataType
 
   constructor() {
-    this.cpu = null
-    this.gpu = null
-    this.ram = null
-    this.hdd = null
-    this.ssd = null
+    this.cpu = {
+      brand: null,
+      model: null,
+      benchmark: null,
+    }
+    this.gpu = {
+      brand: null,
+      model: null,
+      benchmark: null,
+    }
+    this.ram = {
+      brand: null,
+      model: null,
+      benchmark: null,
+    }
+    this.storage = {
+      type: null,
+      brand: null,
+      model: null,
+      benchmark: null,
+    }
   }
 
   public setParts(parts: string, data: FetchDataType): void {
     switch (parts) {
       case 'cpu':
-        this.cpu = data
+        this.cpu.brand = data.Brand
+        this.cpu.model = data.Model
+        this.cpu.benchmark = data.Benchmark
+        console.log(this.cpu)
         break
       case 'gpu':
-        this.gpu = data
+        this.gpu.brand = data.Brand
+        this.gpu.model = data.Model
+        this.gpu.benchmark = data.Benchmark
+        console.log(this.gpu)
         break
       case 'ram':
-        this.ram = data
+        this.ram.brand = data.Brand
+        this.ram.model = data.Model
+        this.ram.benchmark = data.Benchmark
+        console.log(this.ram)
         break
-      case 'hdd':
-        this.hdd = data
-        break
-      case 'ssd':
-        this.ssd = data
-        break
-      default:
-        console.log('not found')
+      case 'storage':
+        this.storage.type = data.Type
+        this.storage.brand = data.Brand
+        this.storage.model = data.Model
+        this.storage.benchmark = data.Benchmark
+        console.log(this.storage)
     }
   }
 
-  public getBenchMark(data: any): number {
-    return data.Benchmark
+  public evalueteAllParts(): boolean {
+    let result = true
+    for (let data in this.cpu) {
+      if (this.cpu[data as keyof CpuDataType] == null) result = false
+    }
+    for (let data in this.gpu) {
+      if (this.gpu[data as keyof CpuDataType] == null) result = false
+    }
+    for (let data in this.ram) {
+      if (this.ram[data as keyof CpuDataType] == null) result = false
+    }
+    for (let data in this.storage) {
+      if (this.storage[data as keyof StorageDataType] == null) result = false
+    }
+    return result
+  }
+
+  public getScoreOfUseForGame(): number {
+    let cpuScore = this.cpu.benchmark! * 0.25
+    let gpuScore = this.gpu.benchmark! * 0.6
+    let ramScore = this.ram.benchmark! * 0.125
+    let storageScore = this.storage.benchmark! * 0.025
+
+    return Math.floor(cpuScore + gpuScore + ramScore + storageScore)
+  }
+
+  public getScoreOfUseForWork(): number {
+    let cpuScore = this.cpu.benchmark! * 0.6
+    let gpuScore = this.gpu.benchmark! * 0.25
+    let ramScore = this.ram.benchmark! * 0.1
+    let storageScore = this.storage.benchmark! * 0.05
+
+    return Math.floor(cpuScore + gpuScore + ramScore + storageScore)
   }
 }
