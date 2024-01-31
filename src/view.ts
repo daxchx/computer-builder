fafddd5d6aec86b2e39c3cad07d6f7e450e30f94
daxchx <daxchx.v1@gmail.com>
2024-01-31 23:41:00 +0900

    Add build computer view

--- src/view.ts
+++ src/view.ts
@@ -6,13 +6,33 @@ export default class View {
     target.append(element)
   }
 
-  public generateComputer(gamingScore: number, workScore: number): void {
-    let element = document.createElement('div')
-    element.innerHTML = `
-      <div>Your Computer</div>
-      <div>Gaming ${gamingScore}%</div>
-      <div>Work ${workScore}%</div>
+  public generateComputer(gamingScore: number, workScore: number, cpuObject: any, gpuObject: any, ramObject: any, storageObject: any): void {
+    document.querySelector<HTMLDivElement>('#computer')!.innerHTML += `
+    <div class="w-80 p-4 border border-gray-200 rounded-lg shrink-0 shadow-md">
+      <div>
+        <div class="text-xs text-gray-400">CPU</div>
+        <div class="text-sm font-bold">${cpuObject.brand}</div>
+        <div class="text-sm font-bold">${cpuObject.model}</div>
+      </div>
+      <div>
+        <div class="text-xs text-gray-400">GPU</div>
+        <div class="text-sm font-bold">${gpuObject.brand}</div>
+        <div class="text-sm font-bold">${gpuObject.model}</div>
+      </div>
+      <div>
+        <div class="text-xs text-gray-400">RAM</div>
+        <div class="text-sm font-bold">${ramObject.brand}</div>
+        <div class="text-sm font-bold">${ramObject.model}</div>
+      </div>
+      <div>
+        <div class="text-xs text-gray-400">STORAGE</div>
+        <div class="text-sm font-bold">${storageObject.type}</div>
+        <div class="text-sm font-bold">${storageObject.brand}</div>
+        <div class="text-sm font-bold">${storageObject.model}</div>
+      </div>
+      <div class="">Gaming ${gamingScore}%</div>
+      <div class="">Work ${workScore}%</div>
+    </div>
     `
-    document.querySelector<HTMLDivElement>('#computer')?.append(element)
   }
 }
